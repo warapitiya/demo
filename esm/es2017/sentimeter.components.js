c1dcc68d138bbb8ad797e627727337383939615f
Malindu Warapitiya <warapitiya93@gmail.com>
2018-10-22 11:12:59 +0800

    more updates

--- esm/es2017/sentimeter.components.js
+++ esm/es2017/sentimeter.components.js
@@ -1,2 +1,2 @@
 // sentimeter: Host Data, ES Module/es2017 Target
-export const SentimeterComponent = ["gt-sentimeter","frmmjhq1",1,[["activatedScreen",16],["blackAndWhite",1,0,"black-and-white",4],["btnColor",1,0,"btn-color",2],["containerHover",16],["el",64],["expandTime",1,0,"expand-time",8],["initialFabTrigger",16],["showModal",16],["showText",16],["showThankYouModal",16],["theme",16],["themeColor",1,0,"theme-color",2]],1];
+export const SentimeterComponent = ["gt-sentimeter","uaea2h70",1,[["activatedScreen",16],["blackAndWhite",1,0,"black-and-white",4],["btnColor",1,0,"btn-color",2],["containerHover",16],["el",64],["expandTime",1,0,"expand-time",8],["initialFabTrigger",16],["showModal",16],["showText",16],["showThankYouModal",16],["theme",16],["themeColor",1,0,"theme-color",2]],1];
